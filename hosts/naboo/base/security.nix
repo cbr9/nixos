c27bb7938ac28a11e7d268015347c250ddd8b700
cbr9 <cabero96@gmail.com>
2025-08-06 00:54:45 +0200

    restore polkit agnt

--- hosts/naboo/base/security.nix
+++ hosts/naboo/base/security.nix
@@ -30,7 +30,8 @@ in
       polkit.enable = true;
     };
 
-    # services.gnome.gnome-keyring.enable = true;
+    services.gnome.gnome-keyring.enable = true;
+    programs.seahorse.enable = true;
 
     services.openssh.enable = cfg.sshd.enable;
     networking.firewall.allowedTCPPorts = [
@@ -39,22 +40,22 @@ in
     ];
     networking.firewall.allowPing = true;
 
-    # systemd = {
-    #   user.services = {
-    #     polkit-gnome-authentication-agent-1 = {
-    #       description = "polkit-gnome-authentication-agent-1";
-    #       wantedBy = [ "graphical-session.target" ];
-    #       wants = [ "graphical-session.target" ];
-    #       after = [ "graphical-session.target" ];
-    #       serviceConfig = {
-    #         Type = "simple";
-    #         ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
-    #         Restart = "on-failure";
-    #         RestartSec = 1;
-    #         TimeoutStopSec = 10;
-    #       };
-    #     };
-    #   };
-    # };
+    systemd = {
+      user.services = {
+        polkit-gnome-authentication-agent-1 = {
+          description = "polkit-gnome-authentication-agent-1";
+          wantedBy = [ "graphical-session.target" ];
+          wants = [ "graphical-session.target" ];
+          after = [ "graphical-session.target" ];
+          serviceConfig = {
+            Type = "simple";
+            ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+            Restart = "on-failure";
+            RestartSec = 1;
+            TimeoutStopSec = 10;
+          };
+        };
+      };
+    };
   };
 }
