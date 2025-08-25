e9b3123c7a7fe2f5d789ea092c1b2e4e2dcaf603
cbr9 <cabero96@gmail.com>
2025-08-25 10:25:40 +0200

    tweak swayidle

--- hosts/naboo/desktop/swayidle/default.nix
+++ hosts/naboo/desktop/swayidle/default.nix
@@ -10,9 +10,8 @@
             command = "${pkgs.swaylock-effects}/bin/swaylock -f";
           }
           {
-            timeout = 60 * 10;
-            command = "${pkgs.niri}/bin/niri msg output DP-2 off && ${pkgs.niri}/bin/niri msg output DP-3 off";
-            resumeCommand = "${pkgs.niri}/bin/niri msg output DP-2 on && ${pkgs.niri}/bin/niri msg output DP-3 on";
+            timeout = 60 * 15;
+            command = "${pkgs.niri}/bin/niri msg action power-off-monitors";
           }
           {
             timeout = 60 * 60;
@@ -22,14 +21,14 @@
         events = [
           {
             event = "before-sleep";
-            command = "${pkgs.niri}/bin/niri msg output DP-2 on && ${pkgs.niri}/bin/niri msg output DP-3 on && ${pkgs.swaylock-effects}/bin/swaylock -f";
+            command = "${pkgs.swaylock-effects}/bin/swaylock -f && ${pkgs.niri}/bin/niri msg action power-off-monitors ";
           }
           {
             # without this, if you resume the system and the monitors are set to use another video input, like HDMI
             # (say you are using your work laptop with the same monitors, but using a different input source),
             # the screens won't output anything, even after you switch the input back to DP
             event = "after-resume";
-            command = "${pkgs.niri}/bin/niri msg output DP-2 on && ${pkgs.niri}/bin/niri msg output DP-3 on";
+            command = "${pkgs.niri}/bin/niri msg action power-on-monitors";
           }
         ];
       };
