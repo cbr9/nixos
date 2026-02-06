d837cbd0316a88a0f87ea1f2eff6935b342fb14c
Andres Cabero Busto <andres.busto@voize.de>
2026-02-06 15:31:03 +0100

    add ssh hostname config

--- modules/ssh/default.nix
+++ modules/ssh/default.nix
@@ -19,6 +19,20 @@ in
     home.sessionVariables = lib.optionalAttrs cfg.enable {
       SSH_AUTH_SOCK = agent;
     };
-    programs.ssh.extraConfig = lib.optionalString cfg.enable "IdentityAgent \"${agent}\"";
+
+    programs.ssh = {
+      enable = true;
+      enableDefaultConfig = false;
+      matchBlocks = {
+        "*" = {
+          forwardAgent = true;
+        };
+        machine-shop-open-toadfish = {
+          user = "cabero";
+          hostname = "machine-shop-open-toadfish";
+        };
+      };
+      extraConfig = lib.optionalString cfg.enable "IdentityAgent \"${agent}\"";
+    };
   };
 }
