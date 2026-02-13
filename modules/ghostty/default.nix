7cc8ed3dbbdc9950f6836bef6e7849f6d00087ea
Andres Cabero Busto <andres.busto@voize.de>
2026-02-13 20:12:09 +0100

    improve ghostty keybinds

--- modules/ghostty/default.nix
+++ modules/ghostty/default.nix
@@ -15,7 +15,7 @@
       enableZshIntegration = true;
       package = if isDarwin then null else pkgs.ghostty;
 
-      settings = {
+      settings = rec {
         theme = "Gruvbox Dark";
         font-size = 17;
         font-family = "SauceCodePro NF";
@@ -32,12 +32,18 @@
 
         macos-option-as-alt = true;
         shell-integration = "fish";
-        keybind = [
-          "super+ctrl+arrow_down=resize_split:down,25"
-          "super+ctrl+arrow_left=resize_split:left,25"
-          "super+ctrl+arrow_right=resize_split:right,25"
-          "super+ctrl+arrow_up=resize_split:up,25"
-        ];
+        keybind =
+          let
+            super_t_action = if window-decoration == "none" then "new_window" else "new_tab"; # ghostty doesn't support tabs when decorations are disabled
+          in
+          [
+            "super+ctrl+arrow_down=resize_split:down,25"
+            "super+ctrl+arrow_left=resize_split:left,25"
+            "super+ctrl+arrow_right=resize_split:right,25"
+            "super+ctrl+arrow_up=resize_split:up,25"
+            "super+t=${super_t_action}"
+            "global:super+enter=new_window"
+          ];
       };
     };
   };
