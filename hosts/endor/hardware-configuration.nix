f4676914881b82c3dc129e490de6886eb1b9cac7
Andres Cabero Busto <andres.busto@voize.de>
2026-03-05 12:44:49 +0100

    add data partition

--- hosts/endor/hardware-configuration.nix
+++ hosts/endor/hardware-configuration.nix
@@ -7,9 +7,13 @@
   hardware.raspberry-pi."4".fkms-3d.enable = true;
   hardware.graphics.enable = true;
 
-  # SD card root filesystem
   fileSystems."/" = {
     device = "/dev/disk/by-label/NIXOS_SD";
+    fsType = "ext4";
+  };
+
+  fileSystems."/data" = {
+    device = "/dev/disk/by-label/DATA";
     fsType = "ext4";
   };
 
